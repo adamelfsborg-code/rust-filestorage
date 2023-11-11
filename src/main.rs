8c95955b718fe2a655f74a10d3e1fc45dd660d55
Adam Elfsborg <adam.elfsborg@mtek.se>
2023-11-11 14:33:50 +0100

    feat: flush db to disk

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@ fn main() {
     let mut arguments = std::env::args().skip(1);
     let key = arguments.next().expect("Key not found");
     let value = arguments.next().expect("Value not found");
-
-    let database = Database::new().unwrap();
-    database.insert(key, value);
+    
+    let mut database = Database::new().unwrap();
+    database.insert(key, value.clone());
+    database.flush().expect("Failed to flush database");
 }
 
 struct Database {
@@ -36,18 +37,19 @@ impl Database {
         Ok(Database { map })
     }
 
-    fn save_database_to_disk(self) {
-        let mut formated_db_string = String::new();
-        for (key, val) in self.map {
-            let contents = format!("{}\t{}\n", key, val);
-            formated_db_string += &contents;
-        }
-        std::fs::write("kv.db", formated_db_string).unwrap(); 
+    fn insert(&mut self, key: String, value: String) {
+        self.map.insert(key, value);
     }
 
-    fn insert(mut self, key: String, value: String) {
-        self.map.insert(key, value);
-        self.save_database_to_disk()
+    fn flush(self) -> Result<(), std::io::Error> {
+        let mut contents = String::new();
+        for (key, val) in &self.map {
+            contents.push_str(key);
+            contents.push('\t');
+            contents.push_str(val);
+            contents.push('\n');
+        }
+        std::fs::write("kv2.db", contents)
     }
 
 }
